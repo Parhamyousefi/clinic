fe4fd8fdccbf5c00b1704eed3bd0fe3af2c96325
Parhamyousefi <parhamyousefi04@gmail.com>
2025-08-20 12:23:26 +0330

    sidebar implemented #22456

--- Clinic.Web/src/app/navbar/navbar.component.ts
+++ Clinic.Web/src/app/navbar/navbar.component.ts
@@ -1,5 +1,25 @@
 import { Component } from '@angular/core';
+export interface imenu {
+  text: string;
+  link: string;
+  roleAccess: number[];
+  icon: string
+}
 
+export const Menu: imenu[] = [
+  { text: "وقت دهی", link: '/', roleAccess: [], icon: '' },
+  { text: "اوقات امروز", link: '/', roleAccess: [], icon: '' },
+  { text: "بیماران", link: '/', roleAccess: [], icon: '' },
+  { text: "صورت حساب ها", link: '/', roleAccess: [], icon: '' },
+  { text: "دریافت ها", link: '/', roleAccess: [], icon: '' },
+  { text: "پرداخت ها", link: '/', roleAccess: [], icon: '' },
+  { text: "کالاهای مصرفی", link: '/', roleAccess: [], icon: '' },
+  { text: "هزینه ها", link: '/', roleAccess: [], icon: '' },
+  { text: "اشخاص", link: '/', roleAccess: [], icon: '' },
+  { text: "نامه ها", link: '/', roleAccess: [], icon: '' },
+  { text: "گزارشات", link: '/', roleAccess: [], icon: '' },
+  { text: "راهنما", link: '/', roleAccess: [], icon: '' },
+];
 @Component({
   selector: 'app-navbar',
   standalone: true,
