b07e80dcad27cec1814ce955ee89644995e15222
Reyhana atashi <reyhan.atashi@outlook.com>
2025-09-06 16:29:00 +0330

    remove dashboard

--- Clinic.Web/src/app/app.routes.ts
+++ Clinic.Web/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'dashboard', component: DashboardComponent },
     { path: 'appointment', component: AppointmentComponent },
 ];
