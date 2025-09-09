54f75478d3862dab83315d89e02654da8d64fe44
AmirHayati <hayati_amir_731@outlook.com>
2025-09-09 13:12:55 +0330

    Implement Create invoice and invoices list #22744

--- Clinic.Web/src/app/_services/invoice.service.ts
+++ Clinic.Web/src/app/_services/invoice.service.ts
@@ -1,11 +1,11 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TreatmentsService {
+export class InvoiceService {
 
   constructor(
     private http: HttpClient
@@ -14,15 +14,15 @@ export class TreatmentsService {
   url = environment.url;
   token: any = localStorage.getItem("token");
 
-  getTodayAppointments(data: any) {
-    const uri = this.url + `api/Treatment/getTodayAppointments`;
+  getInvoices() {
+    const token: any = localStorage.getItem("token");
+    const uri = this.url + `api/Invoice/getInvoices`;
     const httpOptions = {
       headers: new HttpHeaders({
         "Content-Type": "application/json",
-        Authorization: "Bearer " + this.token,
+        Authorization: "Bearer " + token,
       }),
     };
-    return this.http.post(uri, data, httpOptions);
+    return this.http.get(uri, httpOptions);
   }
-  
 }
--- Clinic.Web/src/app/app.routes.ts
+++ Clinic.Web/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { TodayAppointmentsComponent } from './components/today-appointments/today-appointments.component';
+import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
+import { NewInvoiceComponent } from './components/invoice-list/new-invoice/new-invoice.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'appointment', component: AppointmentComponent },
     { path: 'today-appointment', component: TodayAppointmentsComponent },
+    { path: 'invoice-list', component: InvoiceListComponent },
+    { path: 'new-invoice', component: NewInvoiceComponent },
+    
 ];
