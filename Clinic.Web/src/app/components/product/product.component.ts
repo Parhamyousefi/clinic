1d9bdcb6e88d14802d65ecdd6c7e0122f53d4a2e
Mohamadreza <mohamadreza105@outlook.com>
2025-09-24 19:09:56 +0330

    create product and product list components and add form and grid in components #22866

---
+++ Clinic.Web/src/app/components/product/product.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+  constructor(
+  ) { }
+  newContactModel: any;
+  ngOnInit() {
+  }
+}
