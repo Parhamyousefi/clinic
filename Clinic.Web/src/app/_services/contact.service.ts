bba638d33edab17a50f0b7a6531c2c937a1635c2
Nima <mohamadreza105@outlook.com>
2025-10-07 16:09:26 +0330

    Save local changes before merge

--- Clinic.Web/src/app/_services/contact.service.ts
+++ Clinic.Web/src/app/_services/contact.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  public selectedEditContact: Subject<any>;
 
   constructor(
     private http: HttpClient
 
   ) {
-
+    this.selectedEditContact = new Subject();
 
   }
 
@@ -53,6 +55,54 @@ export class ContactService {
       }),
     };
     return this.http.post(uri, data, httpOptions);
+  }
+
+  deleteContact(id) {
+    const token: any = localStorage.getItem("token");
+    const uri = this.url + `api/Contact/deleteContact/` + id;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      }),
+    };
+    return this.http.get(uri, httpOptions);
+  }
+
+  getContactPhone(contactId) {
+    const token: any = localStorage.getItem("token");
+    const uri = this.url + `api/Contact/getContactPhone/` + contactId;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      }),
+    };
+    return this.http.get(uri, httpOptions);
+  }
+
+  savePatientPhone(data) {
+    const uri = this.url + `api/Contact/saveContactPhone`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + this.token,
+        responseType: 'text'
+      }),
+    };
+    return this.http.post(uri, data, httpOptions);
+  }
+
+  deleteContactPhone(contactId) {
+    const token: any = localStorage.getItem("token");
+    const uri = this.url + `api/Contact/deleteContactPhone/` + contactId;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      }),
+    };
+    return this.http.get(uri, httpOptions);
   }
 
 }
