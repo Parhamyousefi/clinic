9fe7a03e717325b858c9bfda6cce76618a5a0bf2
nima ghahremanian <nghahremani11@outlook.com>
2025-10-14 17:48:45 +0330

    Implemented New Controller ANd Endpoints

---
+++ Clinic.Api/Application/DTOs/Report/InvoiceFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Clinic.Api.Application.DTOs.Report
+{
+    public class InvoiceFilterDto
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
---
+++ Clinic.Api/Controllers/ReportController.cs
@@ -0,0 +1,35 @@
+﻿using Clinic.Api.Application.DTOs.Report;
+using Clinic.Api.Application.Interfaces;
+using Clinic.Api.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpPost("getInvoicesByClinic")]
+        [Authorize("Admin","Doctor")]
+        public async Task<IActionResult> GetInvoicesByClinic(InvoiceFilterDto model)
+        {
+            var result = await _reportService.GetInvoicesByService(model);
+            return Ok(result);
+        }
+
+        [HttpPost("getInvoicesByService")]
+        [Authorize("Admin","Doctor")]
+        public async Task<IActionResult> GetInvoicesByService(InvoiceFilterDto model)
+        {
+            var result = await _reportService.GetInvoicesByService(model);
+            return Ok(result);
+        }
+    }
+}
