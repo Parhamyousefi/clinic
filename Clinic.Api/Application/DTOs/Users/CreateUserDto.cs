6fb26ede6b422d290a16ad228a870b93bd8e6aed
nima ghahremanian <nghahremani11@outlook.com>
2025-11-05 10:22:33 +0330

    Last Fix Changes

--- Clinic.Api/Application/DTOs/Users/CreateUserDto.cs
+++ Clinic.Api/Application/DTOs/Users/CreateUserDto.cs
@@ -4,6 +4,15 @@
     {
         public string? Username { get; set; }
         public string? Password { get; set; }
+        public int? IsDoctor { get; set; }
+        public int? ShowTreatmentOnClick { get; set; }
+        public int? CanChangeOldTreatment { get; set; }
+        public int? SuspendReservationDays { get; set; }
+        public int? OutOfRangePatients { get; set; }
+        public string? DoctorSkill { get; set; }
+        public string? Description { get; set; }
+        public int? TitleId { get; set; }
+        public string? Email { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public int? RoleId { get; set; }
