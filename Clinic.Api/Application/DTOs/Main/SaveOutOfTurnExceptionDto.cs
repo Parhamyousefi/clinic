1f21457b88fb0339a93357c983a4f45cca71b73b
nima ghahremanian <nghahremani11@outlook.com>
2025-12-02 16:07:47 +0330

    OutOfTurnException Api's And UserRole

---
+++ Clinic.Api/Application/DTOs/Main/SaveOutOfTurnExceptionDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Clinic.Api.Application.DTOs.Main
+{
+    public class SaveOutOfTurnExceptionDto
+    {
+        public DateTime GrigoryDate { get; set; }
+        public string? StartDate { get; set; }
+        public int PractitionerId { get; set; }
+        public int BusinessId { get; set; }
+        public int OutOfTurn { get; set; }
+        public int EditOrNew { get; set; }
+    }
+}
